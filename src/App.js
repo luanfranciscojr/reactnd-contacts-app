6fd62bc858aad0b91fa23e32360721091bb15697
luan francisco lima fernandes <luanfranciscojr@gmail.com>
2019-03-29 15:51:57 -0300

    contatos apis

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React, { Component } from 'react';
+
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
+import CreateContact from './CreateContact'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    );
-  }
+    state = {
+        contacts: [],
+        screen: 'list'
+    }
+    componentDidMount() {
+        ContactsAPI.getAll()
+            .then((contacts) => {
+                this.setState(() => ({
+                    contacts
+                }))
+            })
+    }
+    removeContact = (contact) => {
+        this.setState((currentState) => ({
+            contacts: currentState.contacts.filter((c) => {
+                return c.id !== contact.id
+            })
+        }))
+
+        ContactsAPI.remove(contact)
+    }
+    render() {
+        return (
+            <div>
+                {this.state.screen === 'list' && (
+                    <ListContacts
+                        contacts={this.state.contacts}
+                        onDeleteContact={this.removeContact}
+                        onNavigate={() => {
+                            this.setState(() => ({
+                                screen: 'create'
+                            }))
+                        }}
+                    />
+                )}
+                {this.state.screen === 'create' && (
+                    <CreateContact />
+                )}
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
